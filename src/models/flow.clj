86b695e4bedc058417ea58ad73a55e8581ca7e60
Kaleb Roncatti <kalebroncatti@gmail.com>
2024-02-28 20:37:30 -0300

    ":rocket: fixing models"

--- src/models/flow.clj
+++ src/models/flow.clj
@@ -1,9 +1,9 @@
 (ns models.flow
   (:require [schema.core :as s]))
 
-
 (s/defschema Flow {(s/required-key :name)        s/Str
                    (s/required-key :description) s/Str
-                   (s/required-key :begin-at)    s/Keyword
-                   (s/required-key :finish-at)   s/Keyword
+                   (s/required-key :begin-at)    s/Str
+                   (s/required-key :finish-at)   s/Str
                    (s/required-key :steps)       [s/Any]})
+
