2c8b4f626baa23ec52ec441a8eda62d52f25645e
Kaleb Roncatti <kalebroncatti@gmail.com>
2024-02-29 19:58:25 -0300

    :rocket: fixing flow model and adding example

--- src/models/flow.clj
+++ src/models/flow.clj
@@ -1,9 +1,12 @@
 (ns models.flow
   (:require [schema.core :as s]))
 
+(s/defschema Tag (s/enum :begin :end))
+
 (s/defschema Step {(s/required-key :name) s/Str
-                   (s/optional-key :next) s/Str
-                   (s/optional-key :previous) s/Keyword})
+                   (s/optional-key :next) [s/Str]
+                   (s/optional-key :previous) [s/Str]
+                   (s/optional-key :tag) Tag})
 
 (s/defschema Flow {(s/required-key :name)        s/Str
                    (s/required-key :description) s/Str
