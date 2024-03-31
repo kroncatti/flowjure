74a262ee97d9db2750534c501183433987270678
Kaleb Roncatti <kalebroncatti@gmail.com>
2024-03-31 16:24:23 -0300

    :memo: replacing typo no logic test

--- test/flowjure/unit/logic/flow_test.clj
+++ test/flowjure/unit/logic/flow_test.clj
@@ -1,4 +1,4 @@
-(ns flowjure.unit.logic.flow_test
+(ns flowjure.unit.logic.flow-test
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [flowjure.logic.flow :as logic.flow]
             [matcher-combinators.test :refer [match?]]
