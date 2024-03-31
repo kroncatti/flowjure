97b36070cc29209d8b49b0ce760bf31f8df33289
Kaleb Roncatti <kalebroncatti@gmail.com>
2024-03-31 19:51:40 -0300

    :memo: moving to test namespace

--- src/flowjure/logic/record.clj
+++ src/flowjure/logic/record.clj
@@ -22,29 +22,3 @@
          {:_id        id
           :path       [(first-step-history flow t)]
           :created-at t}))
-
-(->complete-record (random-uuid)
-                   {:flow-id    "22416b73-cadc-4353-b8f9-327fad9c0952"
-                    :details    {:my-specific-record "example"
-                                 :any-data-we-want   "here"}}
-                   {:name "my-flow"
-                    :description "anything"
-                    :steps [{:name "start"
-                             :next ["step-1" "step-2" "step-3"]
-                             :tag :begin}
-                            {:name "step-1"
-                             :previous ["start"]
-                             :next ["end"]}
-                            {:name "step-2"
-                             :previous ["start"]
-                             :next ["step-4"]}
-                            {:name "step-3"
-                             :previous ["start"]
-                             :next ["step-4"]}
-                            {:name "step-4"
-                             :previous ["step-3" "step-2"]
-                             :next ["end"]}
-                            {:name "end"
-                             :previous ["step-4" "step-1"]
-                             :tag :end}]}
-                   (t/instant))
