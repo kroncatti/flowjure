92272c18a3b567d25edd2fb1ccea315a68cf9d8b
Kaleb Roncatti <kalebroncatti@gmail.com>
2024-05-04 12:07:55 -0300

    :test_tube: Adding tests for record

--- test/integration/flowjure/flow_test.clj
+++ test/integration/flowjure/flow_test.clj
@@ -1,53 +1,33 @@
 (ns integration.flowjure.flow-test
   (:require [integration.helpers.helpers :as helpers]
+            [integration.helpers.mocks :as mocks]
             [state-flow.api :as flow :refer [defflow flow]]
             [state-flow.assertions.matcher-combinators :refer [match?]]))
 
-(def sample-flow {:name        "my-flow"
-                  :description "anything"
-                  :steps       [{:name "start"
-                                 :next ["step-1" "step-2" "step-3"]
-                                 :tag  "begin"}
-                                {:name     "step-1"
-                                 :previous ["start"]
-                                 :next     ["end"]}
-                                {:name     "step-2"
-                                 :previous ["start"]
-                                 :next     ["step-4"]}
-                                {:name     "step-3"
-                                 :previous ["start"]
-                                 :next     ["step-4"]}
-                                {:name     "step-4"
-                                 :previous ["step-3" "step-2"]
-                                 :next     ["end"]}
-                                {:name     "end"
-                                 :previous ["step-4" "step-1"]
-                                 :tag      "end"}]})
-
 (defflow post-flow-test
-         {:init       helpers/start-system!
-          :cleanup    helpers/stop-system!
-          :fail-fast? true}
-         (flow "Post new flow"
-               (match? {:status 200
-                        :body   {:result "success"
-                                 :id     string?}}
-                       (helpers/request! {:method  :post
-                                          :uri     "api/flow"
-                                          :headers {"Accept" "application/json"}
-                                          :body    sample-flow}))))
+  {:init       helpers/start-system!
+   :cleanup    helpers/stop-system!
+   :fail-fast? true}
+  (flow "Post new flow"
+        (match? {:status 200
+                 :body   {:result "success"
+                          :id     string?}}
+                (helpers/request! {:method  :post
+                                   :uri     "api/flow"
+                                   :headers {"Accept" "application/json"}
+                                   :body    mocks/in-sample-flow}))))
 
 (defflow get-flow-by-id-test
-         {:init       helpers/start-system!
-          :cleanup    helpers/stop-system!
-          :fail-fast? true}
-         (flow "Post new flow and retrieve it by id"
-               [flow-id (flow/fmap (comp :id :body) (helpers/request! {:method  :post
-                                                                       :uri     "api/flow"
-                                                                       :headers {"Accept" "application/json"}
-                                                                       :body    sample-flow}))]
-               (match? {:status 200
-                        :body   (assoc sample-flow :id string?)}
-                       (helpers/request! {:method  :get
-                                          :headers {"Accept" "application/json"}
-                                          :uri     (str "api/flow/" flow-id)}))))
+  {:init       helpers/start-system!
+   :cleanup    helpers/stop-system!
+   :fail-fast? true}
+  (flow "Post new flow and retrieve it by id"
+        [flow-id (flow/fmap (comp :id :body) (helpers/request! {:method  :post
+                                                                :uri     "api/flow"
+                                                                :headers {"Accept" "application/json"}
+                                                                :body    mocks/in-sample-flow}))]
+        (match? {:status 200
+                 :body   (assoc mocks/in-sample-flow :id string?)}
+                (helpers/request! {:method  :get
+                                   :headers {"Accept" "application/json"}
+                                   :uri     (str "api/flow/" flow-id)}))))
